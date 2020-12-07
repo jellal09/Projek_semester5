6b1ce785f9144a0cf691ad9c25e2f41596103fc8
ajengayupangesti <ajengayupangesti28@gmail.com>
2020-12-07 09:59:51 +0700

    pesanan saya

--- application/models/M_transaksi.php
+++ application/models/M_transaksi.php
@@ -12,4 +12,32 @@ class M_transaksi extends CI_Model {
     {
         $this->db->insert('detail_transaksi', $detail_data);      
     }
+
+    public function belum_bayar()
+    {
+        $this->db->select('*');
+        $this->db->from('transaksi');
+        $this->db->where('id_pelanggan', $this->session->userdata('id_pelanggan'));
+        $this->db->order_by('id_transaksi', 'desc');
+        return $this->db->get()->result();    
+    }
+    public function detail_pesanan($id_transaksi)
+    {
+        $this->db->select('*');
+        $this->db->from('transaksi');
+        $this->db->where('id_transaksi', $id_transaksi);
+        return $this->db->get()->row();      
+    }
+    public function rekening()
+    {
+        $this->db->select('*');
+        $this->db->from('rekening');
+        return $this->db->get()->result();   
+    }
+    public function upload_bukti_bayar($data)
+    {
+        $this->db->where('id_transaksi', $data['id_transaksi']);
+        $this->db->update('transaksi', $data);  
+    }
+   
 }
