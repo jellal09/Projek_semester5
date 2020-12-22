32c9b0e86dfd97fdb7dc1a0135cc0c2aa8510efe
FitriaAziati999 <56098926+FitriaAziati999@users.noreply.github.com>
2020-12-23 00:46:51 +0700

    perbaikan tampilan di sisi admin

--- application/views/layout/v_nav_admin.php
+++ application/views/layout/v_nav_admin.php
@@ -4,8 +4,8 @@
     <a href="<?= base_url('admin')?>" class="brand-link">
      <!-- <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">-->
-      <i class="fas fa-crown" style="color:pink"></i>
-      <span class="brand-text font-weight-light">Toko Istana</span>
+     
+      <span class="brand-text font-weight-bold my-2" >Toko Istana</span>
     </a>
 
     <!-- Sidebar -->
